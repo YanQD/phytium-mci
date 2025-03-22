0eaedd3992eeddc507b04d3d383bfbd478b0d673
winddevil <1160712160@qq.com>
2025-03-22 11:31:24 +0800

    尝试更新测试框架

--- tests/test.rs
+++ tests/test.rs
@@ -8,57 +8,16 @@ extern crate alloc;
 mod tests {
     use core::time::Duration;
 
-use alloc::vec::Vec;
-use bare_test::{driver::device_tree::get_device_tree, mem::mmu::iomap, print, time::delay};
-use phytium_mci::sd::SdCard; // Adjusted to the correct module path
-use log::*;
-use phytium_mci::{iopad::PAD_ADDRESS, *};
-
-        let mci0 = fdt.find_compatible(&["phytium,mci"]).next().unwrap();
-
-        let reg = mci0.reg().unwrap().next().unwrap();
-
-        info!(
-            "mci0 reg: {:#x},mci0 reg size: {:#x}",
-            reg.address,
-            reg.size.unwrap()
-        );
-
-        let reg_base = iomap((reg.address as usize).into(), reg.size.unwrap());
-
-        let mut mci0 = MCI::new(reg_base);
-
-    let mci_reg_base = iomap((reg.address as usize).into(), reg.size.unwrap());
-
-    let iopad_reg_base = iomap((PAD_ADDRESS as usize).into(), 0x2000);
-
-    let iopad = IoPad::new(iopad_reg_base);
-
-    let mut sdcard = SdCard::example_instance(mci_reg_base,iopad);
-
-    let mut buffer = Vec::new();
-    let _ = sdcard.read_blocks(&mut buffer, 131072+100,1);
-
-    print!("test_work passed\n");
-    for i in 0..buffer.len() {
-        warn!("{:x},{:x},{:x},{:x}",
-                buffer[i] as u8,
-                (buffer[i] >> 8) as u8,
-                (buffer[i] >> 16) as u8,
-                (buffer[i] >> 24) as u8);
-    }
-    assert!(true);
-}
-
-fn sleep(duration: Duration) {
-    spin_on::spin_on(delay(duration));
-}
-
-struct KernelImpl;
-
-impl Kernel for KernelImpl {
-    fn sleep(duration: Duration) {
-        sleep(duration);
+    use bare_test::{
+        globals::{global_val, PlatformInfoKind},
+        mem::mmu::iomap,
+        time::spin_delay,
+    };
+    use log::*;
+    use phytium_mci::{iopad::PAD_ADDRESS, *};
+    #[test]
+    fn test_work() {
+        
     }
 
     fn sleep(duration: Duration) {
